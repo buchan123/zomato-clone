9fd68037ecbc59a5b9090e3d5bb0bee2eb2a9934
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2021-07-10 01:17:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/restaurant')
+def restaurant():
+    return render_template('restaurant.html')
                         
 if __name__ == '__main__':
     app.run()
